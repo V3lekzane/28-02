95cc0a14a5259bbbeba7c0fa65c9ac000af7cbc5
Ehor Abashev <Abashev3467@Gmail.com>
2023-06-11 15:16:18 +0200

    create folders header and app

---
+++ shop-project/src/container/app/app.tsx
@@ -0,0 +1,10 @@
+import Header from 'container/header/header'
+
+const App = () => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+export default App
---
+++ shop-project/src/container/header/header.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+type Props = {}
+const Header = (props: Props) => {
+    return <div className="red">header</div>
+}
+export default Header
