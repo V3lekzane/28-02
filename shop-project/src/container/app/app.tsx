1b25bad9871af55f0a6c3aa60c1c4ade4594d618
Ehor Abashev <Abashev3467@Gmail.com>
2023-06-12 19:24:09 +0200

    add app bar

--- shop-project/src/container/app/app.tsx
+++ shop-project/src/container/app/app.tsx
@@ -1,8 +1,9 @@
 import Header from 'container/header/header'
-
+import CssBaseline from '@mui/material/CssBaseline'
 const App = () => {
     return (
         <>
+            <CssBaseline />
             <Header />
         </>
     )
